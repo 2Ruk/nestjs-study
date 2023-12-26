ed6e9719a7cc1f81083a6ce3b021ef82ce4dfe06
dahoon06 <dahoon226@naver.com>
2023-12-26 15:50:53 +0900

    # Section 1 ~ 4

---
+++ dahoon/server/src/boards/boards.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create.board.dto';
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid,
+      title,
+      description,
+      status: BoardStatus.PULBIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) throw new NotFoundException();
+    return found;
+  }
+
+  deleteBoardById(id: string): void {
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus) {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return this.boards;
+  }
+}
