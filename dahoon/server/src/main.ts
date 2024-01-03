91fa3a77fa40b915ea0f0411b4a71759520aca4d
dahoon06 <dahoon226@naver.com>
2024-01-03 21:35:38 +0900

    feat (Section 10): Config 설정

--- dahoon/server/src/main.ts
+++ dahoon/server/src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const serverConfig = config.get('server');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
-  await app.listen(port);
+  await app.listen(serverConfig.port);
 
-  Logger.log(`Application running on port - ${port}`);
+  Logger.log(`Application running on port - ${serverConfig.port}`);
 }
 bootstrap();
